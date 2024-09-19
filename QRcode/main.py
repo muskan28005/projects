06394f7693ffd795a3dc8bdaf9fb40ba30c1a710
muskan28005 <muskan28005@gmail.com>
2024-09-19 02:45:37 +0100

    Initial commit

---
+++ QRcode/main.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img =  qr.make("www.linkedin.com/in/muskan-malik-87579b255")
+img.save("linkedin.png")
---
+++ qrCode2.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10, border=4,)
+qr.add_data('https://www.google.com')
+qr.make(fit=True)
+img = qr.make_image(fill_color="pink", back_color="black")
+img.save("google.png")
